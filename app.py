bbcb49019344fc439318befa5271b99d89974f38
Drew Menke <menkedrew@gmail.com>
2022-01-08 11:49:24 -0600

    added bokeh chart and fixed dependencies

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+# import plotly.express as px
+from bokeh.plotting import figure, show
 
+st.title("Stock Ticker")
 
-st.title('Uber pickups in NYC')
+x = [1, 2, 3, 4, 5]
+y = [6, 7, 2, 4, 5]
+
+p = figure(
+     title='simple line example',
+     x_axis_label='x',
+     y_axis_label='y')
+
+p.line(x, y, legend_label='Trend', line_width=2)
+
+st.bokeh_chart(p, use_container_width=True)
