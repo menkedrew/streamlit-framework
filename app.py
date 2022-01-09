c5586a08fbe5250f8768023526261e2a8549a536
Drew Menke <menkedrew@gmail.com>
2022-01-09 11:15:33 -0600

    Added the sp500 list to choose from

--- app.py
+++ app.py
@@ -1,25 +1,34 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-# import plotly.express as px
-from bokeh.plotting import figure, show
+from bokeh.plotting import figure, show, output_file
 import yfinance as yf
 from bokeh.models import ColumnDataSource
 from bokeh.models import DatetimeTickFormatter
 from math import pi
 
-df = yf.download("MSFT", start="2021-12-01", end="2021-12-31", group_by="Ticker")
+try:
+    fp = open('SP500_Tickers.txt')
+    sp500 = fp.readlines()
+finally:
+    fp.close()
 
-st.title("Stock Ticker")
+option = st.selectbox('Which Ticker would you like to select?: ', sp500, index=0, help="Choose S&P500 Stock tickers",)
+st.write('You selected: ', option)
+
+df = yf.download(option, start="2021-01-01", end="2021-12-31", group_by="Ticker")
+
+st.title(f"Stock Ticker for: {option}")
 
 cds = ColumnDataSource(df)
 
-p = figure(
-     title='simple line example',
-     x_axis_label='x',
-     y_axis_label='y')
+p = figure(plot_width=800, plot_height=800, title=option, x_axis_label='Time', y_axis_label='Points')
 
-p.line(x="Date", y="Close", source=cds, legend_label='Trend', line_width=2)
+p.outline_line_width = 7
+p.outline_line_alpha = 0.3
+p.outline_line_color = "red"
+
+p.line(x="Date", y="Close", source=cds, legend_label='Close price: '+ option, line_width=2)
 
 p.xaxis.formatter=DatetimeTickFormatter(
         hours=["%d %B %Y"],
@@ -28,5 +37,7 @@ p.xaxis.formatter=DatetimeTickFormatter(
         years=["%d %B %Y"],
 )
 p.xaxis.major_label_orientation = pi/4
+
+p.toolbar.autohide = True
 
 st.bokeh_chart(p, use_container_width=True)
